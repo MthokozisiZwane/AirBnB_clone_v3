767adba61ce299218a03605374d70ae81cca5b8a
MthokozisiZwane <mthokozisi.zwane1999@gmail.com>
2024-01-09 15:48:42 -0800

    place amenities

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""
+amenity api
+"""
+
+
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET', 'POST'],
+                 strict_slashes=False)
+def place_amenities(place_id):
+    """Handle GET and POST requests on /places/<place_id>/amenities"""
+    place = storage.get(Place, place_id)
+
+    if place is None:
+        abort(404)
+
+    if request.method == 'GET':
+        amenities = [amenity.to_dict() for amenity in place.amenities]
+        return jsonify(amenities)
+
+    if request.method == 'POST':
+        amenity_id = request.get_json().get('amenity_id')
+
+        if amenity_id is None:
+            abort(400, 'Missing amenity_id')
+
+        amenity = storage.get(Amenity, amenity_id)
+
+        if amenity is None:
+            abort(404)
+
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
+
+        place.amenities.append(amenity)
+        storage.save()
+        return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """Handle DELETE requests on /places/<place_id>/amenities/<amenity_id>"""
+    place = storage.get(Place, place_id)
+
+    if place is None:
+        abort(404)
+
+    amenity = storage.get(Amenity, amenity_id)
+
+    if amenity is None:
+        abort(404)
+
+    if amenity not in place.amenities:
+        abort(404)
+
+    place.amenities.remove(amenity)
+    storage.save()
+    return jsonify({}), 200
